c97a21d76af5a906567784d2128b208287debde1
xueran <873160812@qq.com>
2020-05-14 17:04:57 +0800

    初始化

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by xueranit on 2020/5/14.
+//  Copyright © 2020 xr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ gitTest/gitTest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  gitTest
+//
+//  Created by xueranit on 2020/5/14.
+//  Copyright © 2020 xr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
